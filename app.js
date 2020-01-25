99fc252c60f8f244ff144372c0077854c4b9004b
Jose Santiago <gallojsantiago@gmail.com>
2020-01-25 09:20:30 -0600

    how to make it realtime?

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ var {google} = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 
 var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
-// Change this dir later
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
+var TOKEN_PATH = TOKEN_DIR + 'discord-yt-likecount-bot.json';
 
 // Load client secrets from a local file (client_secret.json)
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
@@ -23,13 +22,6 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
   authorize(JSON.parse(content), getChannel);
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
 function authorize(credentials, callback) {
   var clientSecret = credentials.installed.client_secret;
   var clientId = credentials.installed.client_id;
@@ -47,14 +39,6 @@ function authorize(credentials, callback) {
   });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
 function getNewToken(oauth2Client, callback) {
   var authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
@@ -79,11 +63,6 @@ function getNewToken(oauth2Client, callback) {
   });
 }
 
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
 function storeToken(token) {
   try {
     fs.mkdirSync(TOKEN_DIR);
@@ -98,38 +77,24 @@ function storeToken(token) {
   });
 }
 
-/**
- * Lists the names and IDs of up to 10 files.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
 function getChannel(auth) {
   var service = google.youtube('v3');
-  service.videos.list({
-    auth: auth,
-    part: 'snippet,contentDetails,statistics',
-    id: 'G1WlOlat_UI' // Random video/livestream
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var video = response.data.items;
-    var info = 'This videos ID is ' + video[0].id + ' Its title is ' + video[0].snippet.title + ' and it has ' + video[0].statistics.viewCount + 
-    ' views currenly. The number of likes are ' + video[0].statistics.likeCount;
-    if (video.length == 0) {
-      console.log('No channel found.');
-    } else {
-      console.log(info);
-    
-      // Discord bot
-      client.on('ready', () => {
-          client.channels.find(x => x.name === 'general').send("I'd just like to interject for a moment. What you're referring to as Linux, " +
-          "is in fact, GNU/Linux,or as I've recently taken to calling it, GNU plus Linux.")
-          client.channels.find(x => x.name === 'general').send(info);
-      });
-      client.login(botSettings.token);
-    }
-  });
+    service.videos.list({
+      auth: auth,
+      part: 'snippet,contentDetails,statistics',
+      id: '8dykkHZBJF4' // Random video/livestream
+    }, function(err, response) {
+      if (err) {
+        console.log('The API returned an error: ' + err);
+        return;
+      }
+      var video = response.data.items;
+      if (video.length == 0) {
+        console.log('No video found.');
+      } else {
+        console.log("The likes on this video: '" + video[0].snippet.title + "' is " + video[0].statistics.likeCount);
+      }
+    });
 }
+
 
